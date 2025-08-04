c36fce7498963a49b1dc0418a0c5f7ba5f34a331
Daniela <suarezdaniela115@gmail.com>
2025-08-04 04:16:40 -0600

    Agrega parte 2 y 3 de la historia

--- historia.py
+++ historia.py
@@ -1,3 +1,5 @@
 print("Parte 1: Juan salió de su casa muy temprano.")
 # Parte 2
+print("Parte 2: Se encontró con un dragón en la entrada del bosque.")  # Integrante 2
 # Parte 3 
+print("Parte 3: Al final, el dragón resultó ser su mascota perdida.")  # Integrante 3
